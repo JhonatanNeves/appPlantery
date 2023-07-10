0fb0a819ea99722e7336e4c7b27e7c3fcdfd6c8e
Jhonatan Neves <nevesjn2@gmail.com>
2023-07-10 20:10:07 -0300

    feat-dialog

--- app/src/main/java/com/example/appplantery/register/view/RegisterPhotoFragment.kt
+++ app/src/main/java/com/example/appplantery/register/view/RegisterPhotoFragment.kt
@@ -6,13 +6,32 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.appplantery.R
+import com.example.appplantery.common.view.CustomDialog
 
 class RegisterPhotoFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_register_photo, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val customDialog = CustomDialog(requireContext())
+
+        // customDialog.setTitle(R.string.app_name)
+        customDialog.addButton(R.string.photo, R.string.gallery) {
+            when(it.id) {
+                R.string.photo -> {
+
+                }
+                R.string.gallery -> {
+
+                }
+            }
+        }
+        customDialog.show()
+    }
+
 }
