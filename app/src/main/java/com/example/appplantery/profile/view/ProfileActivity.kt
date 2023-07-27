babcec8b0e7240f568d44829eda1587202200c00
Jhonatan Neves <nevesjn2@gmail.com>
2023-07-27 19:55:22 -0300

    feat-profile-statusBar

--- app/src/main/java/com/example/appplantery/profile/view/ProfileActivity.kt
+++ app/src/main/java/com/example/appplantery/profile/view/ProfileActivity.kt
@@ -1,15 +1,26 @@
 package com.example.appplantery.profile.view
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.WindowInsetsController
+import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.example.appplantery.R
 
 class ProfileActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        window.insetsController?.setSystemBarsAppearance(
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.orange_dull)
 
         val toolbar: Toolbar = findViewById(R.id.activity_toolbar)
         setSupportActionBar(toolbar)
