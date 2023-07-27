8514192acb710d5ac8c51d35be089c98e0c1ac7c
Jhonatan Neves <nevesjn2@gmail.com>
2023-07-26 22:19:23 -0300

    feat-profile-menus

--- app/src/main/java/com/example/appplantery/profile/view/ProfileListFragment.kt
+++ app/src/main/java/com/example/appplantery/profile/view/ProfileListFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.appplantery.R
 
 class ProfileListFragment : Fragment () {
@@ -15,4 +18,36 @@ class ProfileListFragment : Fragment () {
     ): View? {
         return inflater.inflate(R.layout.fragment_profile_list, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val rv = view.findViewById<RecyclerView>(R.id.profile_rv)
+        rv.layoutManager = LinearLayoutManager(context)
+        rv.adapter = PostAdapter()
+    }
+
+    private class PostAdapter : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+            return PostViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_itens_menu, parent, false)
+            )
+        }
+
+        override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+            holder.bind(R.drawable.ic_insta_profile)
+        }
+
+        override fun getItemCount(): Int {
+            return 4
+        }
+
+        private class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(image: Int) {
+                itemView.findViewById<ImageView>(R.id.imageProfile).setImageResource(image)
+            }
+        }
+
+    }
+
 }
