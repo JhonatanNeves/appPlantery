17c8bbcae76357be849ff42dff8c3e0674bd6920
Jhonatan <nevesjn2@gmail.com>
2023-12-01 22:04:27 -0300

    att

---
+++ app/src/main/java/com/example/appplantery/home/view/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.example.appplantery.home.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.example.appplantery.R
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
